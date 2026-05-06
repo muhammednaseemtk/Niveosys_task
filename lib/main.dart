b5053a84c6024a9e7afe38bddadd55579d6a109c
Naseem <muhammednaseemtk000@gmail.com>
2026-05-06 15:47:30 +0530

    completed for this task in niveosys

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:niveosys/core/routes/app_routes.dart';
-import 'package:niveosys/features/dashboard/view/dashboard_screen.dart';
 import 'package:niveosys/features/splash/view/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'features/auth/controller/auth_controller.dart';
